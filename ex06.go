d5f2cb7cdc0cdb1bc850126b4a4a6c0a204fcb58
neyzar <lucas.narguet451@gmail.com>
2024-12-25 23:25:12 +0100

    ajout perso2

--- ex06.go
+++ ex06.go
@@ -14,6 +14,10 @@ func main() {
 
 	perso1.name = "lucas"
 	perso1.age = 21
-	perso1.adresse = "1 rue de la pai"
+	perso1.adresse = "1 rue de la paix"
+
+	perso2.name = "nina"
+	perso2.age = 23
+	perso2.adresse = "1 rue napoleons"
 
 }
