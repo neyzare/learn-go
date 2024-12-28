8bd02dd1641b8ec95d3a8f78d9422293918330c6
neyzar <lucas.narguet451@gmail.com>
2024-12-28 19:10:44 +0100

    ajout d'une methode dans struct

--- struct.go
+++ struct.go
@@ -13,6 +13,19 @@ func (p Personnage) great() {
 	fmt.Println("Bonjour je m'appelle ", p.name, "et j'ai ", p.age,  "ans")
 }
 
+type Rectangle struct {
+	width float64
+	height float64
+}
+
+func (r Rectangle) air() float64 {
+	 return r.width * r.height
+}
+
+func (r Rectangle) perimetre() (float64) {
+	return 2 * r.width * r.height
+}
+
 func main() {
 	var perso1 Personnage
 	var perso2 Personnage
@@ -25,9 +38,15 @@ func main() {
 	perso2.age = 23
 	perso2.adresse = "1 rue napoleons"
 
+	rectangle1 := Rectangle{width: 34,height: 53}
+	rectangle2 := Rectangle{width: 543, height: 43}
+
 	fmt.Println(perso1)
 	fmt.Println(perso2)
 	perso1.great()
+
+	fmt.Println(rectangle1.air())
+	fmt.Println(rectangle2.perimetre())
 
 }
 
